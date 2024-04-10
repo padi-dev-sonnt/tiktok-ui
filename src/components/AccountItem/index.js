557d5daf1916e161809855e1259e0a0450ec0f96
padi-dev-sonnt <sonnt@paditech.com>
2024-04-10 15:26:56 +0700

    110&111: Add PropTypes Validation Global

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
@@ -22,6 +23,10 @@ function AccountItem({ data }) {
             </div>
         </Link>
     );
+}
+
+AccountItem.propTypes = {
+    data: PropTypes.object.isRequired
 }
 
 export default AccountItem;
